c9a9b3d60f984500c6998365281726f67c2adfd0
primer900 <cjprocak13@gmail.com>
2016-10-31 22:38:42 -0500

    Created 8 red squares on screen

--- CheckerBoard.cpp
+++ CheckerBoard.cpp
@@ -5,6 +5,19 @@
 #include <GL/glut.h>
 
 
+void View() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double winHt = 1.0;
+	glOrtho(-winHt*72/48.0, winHt*72/48.0, -winHt, winHt, 0.1, 100.0);
+}
+
+void Camera() {
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.5, 1.5, 2, 0, 0, 0, 0.0, 1.0, 0.0);
+}
+
 void DrawCheckerPiece() {
 	GLfloat mat_ambient[] = { 1.0f, 0.0f, 0.0f, 1.0f};
 	GLfloat mat_diffuse[] = {1.0f, 0.0f, 0.0f, 1.0f};
@@ -20,25 +33,33 @@ void DrawCheckerPiece() {
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 
-	//view
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	double winHt = 1.0;
-	glOrtho(-winHt*64/48.0, winHt*64/48.0, -winHt, winHt, 0.1, 100.0);
-
-	//Camera
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(2.3, 1.3, 2, 0, 0.25, 0, 0.0, 1.0, 0.0);
+	View();
+	Camera();
 
 	//Drawing
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glPushMatrix();
-	glTranslated(0, 1, 0);
-	glScaled(0.1, 0.15, 0.1);
-	glutSolidCube(1.0);
-	glPopMatrix();
+	double x = -1.5;
+	while(x < 0.5){
+		glPushMatrix();
+		glTranslated(x + 0.1, -1, 0);
+		glScaled(0.1, 0.1, 0.1);
+		glutSolidCube(1.0);
+		glPopMatrix();
+
+		x += 0.5;
+	}
+
+	x = -1;
+	while(x < 1) {
+		glPushMatrix();
+		glTranslated(x + 0.1, -0.5, 0);
+		glScaled(0.1, 0.1, 0.1);
+		glutSolidCube(1.0);
+		glPopMatrix();
+
+		x += 0.5;
+	}
 
 	glFlush();
 }
@@ -56,7 +77,7 @@ int main(int argc, char** argv){
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE);
 	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glViewport(0, 0, 640, 480);
+	glViewport(0, 0, 720, 480);
 	glutMainLoop();
 	return 0;
 }
