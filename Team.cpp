4d332b54aa3d778c9440b6e3126cfd1b6781061b
primer900 <cjprocak13@gmail.com>
2016-11-19 20:46:40 -0600

    Added third row of pieces for each team

--- Team.cpp
+++ Team.cpp
@@ -13,51 +13,66 @@
 CameraViewUtility cameraViewTeam;
 FileUtility fileUtility;
 
-Piece * BlackPieces[8];
-Piece * RedPieces[8];
+Piece * BlackPieces[12];
+Piece * RedPieces[12];
 
 void DrawRedTeam() {
 	cameraViewTeam.Lights(1.0f, 0.0f, 0.0f);
 
 	for(int x = 0; x < 4; x++) {
-		RedPieces[2 * x]->SetPosition(0.5 * x - 1, -0.8, 0);
-		RedPieces[2 * x]->DrawPiece();
-		fileUtility.WriteToFile(RedPieces[2 * x]->x, RedPieces[2 * x]->y, RedPieces[2 * x]->z, "RedMove0.dat");
-		RedPieces[2 * x + 1]->SetPosition(0.5 * x - 0.75, -0.8, -0.25);
-		RedPieces[2 * x + 1]->DrawPiece();
-		fileUtility.WriteToFile(RedPieces[2 * x + 1]->x, RedPieces[ 2 * x + 1]->y, RedPieces[2 * x + 1]->z, "RedMove0.dat");
+		RedPieces[x]->SetPosition(0.5 * x - 1, -0.8, 0);
+		RedPieces[x]->DrawPiece();
+		fileUtility.WriteToFile(RedPieces[x]->x, RedPieces[x]->y, RedPieces[x]->z, "RedMove0.dat");
+	}
+	for(int x = 0; x < 4; x++) {
+		RedPieces[x + 4]->SetPosition(0.5 * x - 0.75, -0.8, -0.25);
+		RedPieces[x + 4]->DrawPiece();
+		fileUtility.WriteToFile(RedPieces[x + 4]->x, RedPieces[x + 4]->y, RedPieces[x + 4]->z, "RedMove0.dat");
+	}
+	for(int x = 0; x < 4; x++) {
+		RedPieces[x + 8]->SetPosition(0.5 * x - 1, -0.8, -0.5);
+		RedPieces[x + 8]->DrawPiece();
+		fileUtility.WriteToFile(RedPieces[x + 8]->x, RedPieces[x + 8]->y, RedPieces[x + 8]->z, "RedMove0.dat");
 	}
 }
 
 void DrawBlackTeam(bool moveBlackPiece) {
 	cameraViewTeam.Lights(0.0f, 0.0f, 0.0f);
 
-	if(!moveBlackPiece)
+	if(!moveBlackPiece) {
 		for(int x = 0; x < 4; x++) {
-			BlackPieces[2 * x]->SetPosition(0.5 * x - 1, -0.8, -1.5);
-			BlackPieces[2 * x]->DrawPiece();
-			fileUtility.WriteToFile(BlackPieces[2 * x]->x, BlackPieces[2 * x]->y, BlackPieces[2 * x]->z, "BlackMove0.dat");
-			BlackPieces[2 * x + 1]->SetPosition(0.5 * x - 0.75, -0.8, -1.75);
-			BlackPieces[2 * x + 1]->DrawPiece();
-			fileUtility.WriteToFile(BlackPieces[2 * x + 1]->x, BlackPieces[2 * x + 1]->y, BlackPieces[2 * x + 1]->z, "BlackMove0.dat");
+			BlackPieces[x]->SetPosition(0.5 * x - 0.75, -0.8, -1.25);
+			BlackPieces[x]->DrawPiece();
+			fileUtility.WriteToFile(BlackPieces[x]->x, BlackPieces[x]->y, BlackPieces[x]->z, "BlackMove0.dat");
 		}
-	else
-		for(int x = 0; x < 4; x++) {
-			if(x == 0) {
-				BlackPieces[2 * x]->SetPosition(0.5 * x - 1, - 0.8, -1.25);
-				BlackPieces[2 * x]->DrawPiece();
-			}
-			else {
-				BlackPieces[2 * x]->SetPosition(0.5 * x - 1, -0.8, -1.5);
-				BlackPieces[2 * x]->DrawPiece();
-			}
-			BlackPieces[2 * x + 1]->SetPosition(0.5 * x - 0.75, - 0.8, -1.75);
-			BlackPieces[2 * x + 1]->DrawPiece();
+	}
+
+	else {
+		BlackPieces[0]->SetPosition(-0.75, -0.8, -1);
+		BlackPieces[0]->DrawPiece();
+		fileUtility.WriteToFile(BlackPieces[0]->x, BlackPieces[0]->y, BlackPieces[0]->z, "BlackMove0.dat");
+
+		for(int x = 1; x < 4; x++) {
+			BlackPieces[x]->SetPosition(0.5 * x - 0.75, -0.8, -1.25);
+			BlackPieces[x]->DrawPiece();
+			fileUtility.WriteToFile(BlackPieces[x]->x, BlackPieces[x]->y, BlackPieces[x]->z, "BlackMove0.dat");
 		}
+	}
+
+	for(int x = 0; x < 4; x++) {
+		BlackPieces[x + 4]->SetPosition(0.5 * x - 1, -0.8, -1.5);
+		BlackPieces[x + 4]->DrawPiece();
+		fileUtility.WriteToFile(BlackPieces[x + 4]->x, BlackPieces[x + 4]->y, BlackPieces[x + 4]->z, "BlackMove0.dat");
+	}
+	for(int x = 0; x < 4; x++) {
+		BlackPieces[x + 8]->SetPosition(0.5 * x - 0.75, -0.8, -1.75);
+		BlackPieces[x + 8]->DrawPiece();
+		fileUtility.WriteToFile(BlackPieces[x + 8]->x, BlackPieces[x + 8]->y, BlackPieces[x + 8]->z, "BlackMove0.dat");
+	}
 }
 
 void InitializePieces() {
-	for(int i = 0; i < 8; i++) {
+	for(int i = 0; i < 12; i++) {
 		RedPieces[i] = new Piece();
 		BlackPieces[i] = new Piece();
 	}
